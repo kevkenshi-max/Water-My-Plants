9589058f7f7a0694b2b787818b271feb84dd2121
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-04-06 22:33:40 -0700

    kevin-lee

--- data/seeds/plants.js
+++ data/seeds/plants.js
@@ -1,4 +1,20 @@
 
 exports.seed = function(knex) {
-
+  return knex('plants').truncate()
+    .then(function () {
+        return knex('plants').insert([
+            {
+                nickname: 'sakura',
+                species: 'cherry blossoms',
+                h20_frequency: '3 times a day',
+                user_id: 1
+            },
+            {
+                nickname: 'panda',
+                species: 'bamboo',
+                h20_frequency: 'once a day',
+                user_id: 2
+            }
+        ])
+    })
 };
--- data/seeds/users.js
+++ data/seeds/users.js
@@ -1,4 +1,10 @@
 
 exports.seed = function(knex) {
-
+    return knex('users').truncate()
+      .then(function () {
+          return knex('users').insert([
+              {username: 'foo', password: 'bar', phonenumber: '2139998888'},
+              {username: 'jane', password: '1234', phonenumber: '2138889999'}
+          ])
+      })
 };
